2b46fe1df21654fc8561309c801f4a659be21934
Kedyn Macedonio <mkedyn@yahoo.com>
2017-04-01 12:13:24 -0700

    Main file comments

--- main.ts
+++ main.ts
@@ -2,8 +2,11 @@ import {SimpleTwitchBot} from "./core/SimpleTwitchBot";
 import {RootModule} from "./modules/RootModule";
 
 //Get your OAuth key from: https://twitchapps.com/tmi/
+//SimpleTwitchBot(Bot name, OAuth key, channel list, reconnect, debug);
 let my_bot = new SimpleTwitchBot("SimpleTwitchBot","oauth:???",["SimpleTwitchBot", "MyChannel"],true,true);
 
+
+//Add the Root Module
 my_bot.addModule(new RootModule(my_bot, "Me"));
 
 my_bot.connect();
